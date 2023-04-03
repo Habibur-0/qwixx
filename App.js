9678f90ba30bd38e535c126008d71308ea92ed48
Habibur-0 <Habibrahman048@gmail.com>
2023-04-03 13:30:11 +0100

    bug

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import WelcomeScreen from '../src/screens/Welcome';
-import MainScreen from '../src/screens/Main';
+import WelcomeScreen from '/src/screens/Welcome';
+import MainScreen from '/src/screens/Main';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,9 +12,9 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="Main" component={MainScreen} />
+
 
       </Stack.Navigator>
     </NavigationContainer>
